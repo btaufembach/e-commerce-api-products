1aeecdf48086506a24934c61552a7ca7b74d1394
btaufembach <87210810+btaufembach@users.noreply.github.com>
2021-09-16 21:45:33 -0300

    add metodo delete, filtros

--- src/main/java/br/senac/devweb/api/product/categoria/CategoriaService.java
+++ src/main/java/br/senac/devweb/api/product/categoria/CategoriaService.java
@@ -1,9 +1,13 @@
 package br.senac.devweb.api.product.categoria;
 
+import com.querydsl.core.types.Predicate;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 @AllArgsConstructor
@@ -19,5 +23,15 @@ public class CategoriaService {
                 .status(Categoria.Status.ATIVO)
                 .build());
 
+    }
+
+    public List<Categoria> getAllCaregoria(Predicate filter) {
+        return this.categoriaRepository.findAll(filter);
+    }
+
+    public void deleteCategoria(Long id) {
+        Categoria categoria = this.categoriaRepository.findById(id).get();
+        categoria.setStatus(Categoria.Status.INATIVO);
+        this.categoriaRepository.save(categoria);
     }
 }
